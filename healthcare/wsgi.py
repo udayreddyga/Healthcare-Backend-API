d260d1c526b9b98ec0b2e0963d0aaffb48f218aa
udayreddyga <gangasaniudayreddy99@gmail.com>
2026-01-05 23:29:20 +0530

    Auto create superuser for Render

--- healthcare/wsgi.py
+++ healthcare/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for healthcare project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'healthcare.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "healthcare.settings")
 
 application = get_wsgi_application()
+
+try:
+    from .auto_superuser import create_superuser
+    create_superuser()
+except Exception:
+    pass
